5d519fde82075b558cb8f8fd7e0928d13b21b304
Rushi Shah <2016rshah@gmail.com>
2014-12-13 17:12:14 -0500

    Board class. Needs work

---
+++ python/game-of-life-2.py/Board.py
@@ -0,0 +1,32 @@
+from Cell import Cell
+
+class Board(object):
+	def __init__(self, r, c):
+		self.rows = r
+		self.cols = c
+		self.board = []
+	def __repr__(self):
+		s = ""
+		for i in range(self.rows):
+			for j in range(self.cols):
+				s+=self.board[i][j].__repr__() + " "
+			s+="\n"
+		return s
+	def makeEmptyBoard(self):
+		for i in range(self.rows):
+			self.board.append([])
+		for i in range(self.rows):
+			for j in range(self.cols):
+				self.board[i].append(Cell(i, j))
+	def killCell(self, i, j):
+		self.board[i][j].alive = False
+	def reviveCell(self, i, j):
+		self.board[i][j].alive = True
+
+
+"""
+	To Do:
+		check if in bounds method for coordinates
+		calculate all neighbours of one cell 
+		toggleAsNeeded method
+"""
