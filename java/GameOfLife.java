770b86127392e7e9811f181079a8984f19e8f172
Rushi Shah <2016rshah@gmail.com>
2014-12-13 13:45:22 -0500

    Testing methods

--- java/GameOfLife.java
+++ java/GameOfLife.java
@@ -1,14 +1,9 @@
 public class GameOfLife{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        Board board = new Board(10, 10);
-        for(int i = 0; i<board.getRows(); i++)
-        {
-            for(int j = 0; j<board.getCols(); j++)
-            {
-                System.out.print(board.setCell(i, j, new Cell(i, j, false)) + "\t");
-            }
-            System.out.println();
-        }
-    }
+   public static void main(String[] args){
+      System.out.println("Hello World");
+      Board board = new Board(5, 5);
+      board.initialize();
+	  board.reviveCell(2, 3);
+      System.out.println(board);
+   }
 }
