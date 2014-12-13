486a811ade21af3f95a01283c4b9904c41962a73
Rushi Shah <2016rshah@gmail.com>
2014-12-13 13:44:31 -0500

    Initialize, revive/kill, toString

--- java/Board.java
+++ java/Board.java
@@ -1,27 +1,58 @@
 public class Board{
-    private Cell[][] board;
-    private int rows;
-    private int cols;
-    
-    public Board(int r, int c)
-    {
-        rows = r;
-        cols = c;
-        board = new Cell[rows][cols];
-    }
-    public Cell getCell(int r, int c)
-    {
-        return board[r][c];
-    }
-    public Cell setCell(int row, int col, Cell c)
-    {
-        board[row][col] = c;
-        return board[row][col];
-    }
-    public int getRows(){
-        return rows;
-    }
-    public int getCols(){
-        return cols;
-    }
+   private Cell[][] board;
+   private int rows;
+   private int cols;
+   
+   public Board(int r, int c)
+   {
+      rows = r;
+      cols = c;
+      board = new Cell[rows][cols];
+   }
+   public Cell getCell(int r, int c)
+   {
+      return board[r][c];
+   }
+   public Cell setCell(int row, int col, Cell c)
+   {
+      board[row][col] = c;
+      return board[row][col];
+   }
+   public int getRows(){
+      return rows;
+   }
+   public int getCols(){
+      return cols;
+   }
+   public void killCell(int i, int j)
+   {
+      this.getCell(i, j).kill();
+   }
+   public void reviveCell(int i, int j)
+   {
+      this.getCell(i, j).revive();
+   }
+   public void initialize()
+   {
+      for(int i = 0; i<this.getRows(); i++)
+      {
+         for(int j = 0; j<this.getCols(); j++)
+         {
+            this.setCell(i, j, new Cell(i, j, false));
+         }
+      }
+   }
+   public String toString()
+   {
+      String s = "";
+      for(int i = 0; i<this.getRows(); i++)
+      {
+         for(int j = 0; j<this.getCols(); j++)
+         {
+            s = s + this.getCell(i, j).toString() + " ";
+         }
+         s = s + "\n";
+      }
+      return s;
+   }
 }
