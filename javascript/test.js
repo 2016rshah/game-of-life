4de3d714392f6f1f68eaf034c5bf1cafe1117ad3
Rushi Shah <2016rshah@gmail.com>
2014-12-15 18:17:46 -0500

    Test modules, get all commit dates and push formatted into array

---
+++ javascript/test.js
@@ -0,0 +1,68 @@
+// var express = require('express.io')
+// var app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+// var server = app.listen(3000, function () {
+
+//   var host = server.address().address
+//   var port = server.address().port
+
+//   console.log('Example app listening at http://%s:%s', host, port)
+
+// })
+
+
+
+
+
+var GitHubApi = require("github");
+var isodate = require("isodate");
+var datesContributed = []
+// // Read a date string
+// var date = isodate("2011-08-20T19:39:52Z");
+// console.log(date);
+
+
+var github = new GitHubApi({
+    // required
+    version: "3.0.0",
+});
+github.authenticate({
+	type: "oauth", 
+	token: "YOUR_TOKEN_HERE"
+})
+github.repos.getAll({
+    // optional:
+    // headers: {
+    //     "cookie": "blahblah"
+    // },
+    user: "2016rshah",
+    type: "public"
+}, function(err, res) {
+	if(err)
+		console.log(err);
+    var data = res
+    for(var i = 0; i<data.length; i++)
+    {
+    	github.repos.getCommits({
+    		user: "2016rshah", 
+    		repo: data[i].name
+    	}, function(err, result){
+    		if(err)
+    			console.log(err)
+    		else
+    		{
+    			var commits = result
+    			for(var i = 0; i<commits.length; i++)
+    			{
+    				//console.log(commits[i].commit)
+    				console.log(isodate(commits[i].commit.committer.date))
+    				datesContributed.push(isodate(commits[i].commit.commiter.date))
+    			}
+    		}
+    	})
+    }
+});
