7decd2c98a6c727421bbbdb269cb383dccfe2a18
Rushi Shah <2016rshah@gmail.com>
2014-12-16 20:02:04 -0500

    Token, callback, etc.

--- javascript/test.js
+++ javascript/test.js
@@ -20,49 +20,55 @@
 
 var GitHubApi = require("github");
 var isodate = require("isodate");
-var datesContributed = []
-// // Read a date string
-// var date = isodate("2011-08-20T19:39:52Z");
-// console.log(date);
+var fs = require('fs')
+var TOKEN = ""
+var path = __dirname + "/private/secrets.txt"
+fs.readFile(path, 'utf8', function(err, data){
+    TOKEN = data.split("\n")[0]
+    var datesContributed = []
+    // // Read a date string
+    // var date = isodate("2011-08-20T19:39:52Z");
+    // console.log(date);
 
 
-var github = new GitHubApi({
-    // required
-    version: "3.0.0",
-});
-github.authenticate({
-	type: "oauth", 
-	token: "YOUR_TOKEN_HERE"
+    var github = new GitHubApi({
+        // required
+        version: "3.0.0",
+    });
+    github.authenticate({
+        type: "oauth", 
+        token: TOKEN
+    })
+    github.repos.getAll({
+        // optional:
+        // headers: {
+        //     "cookie": "blahblah"
+        // },
+        user: "2016rshah",
+        type: "public"
+    }, function(err, res) {
+        if(err)
+            console.log(err);
+        var data = res
+        for(var i = 0; i<data.length; i++)
+        {
+            github.repos.getCommits({
+                user: "2016rshah", 
+                repo: data[i].name
+            }, function(err, result){
+                if(err)
+                    console.log(err)
+                else
+                {
+                    var commits = result
+                    for(var i = 0; i<commits.length; i++)
+                    {
+                        //console.log(commits[i].commit)
+                        console.log(isodate(commits[i].commit.committer.date))
+                        datesContributed.push(isodate(commits[i].commit.committer.date))
+                    }
+                }
+            })
+        }
+    });
 })
-github.repos.getAll({
-    // optional:
-    // headers: {
-    //     "cookie": "blahblah"
-    // },
-    user: "2016rshah",
-    type: "public"
-}, function(err, res) {
-	if(err)
-		console.log(err);
-    var data = res
-    for(var i = 0; i<data.length; i++)
-    {
-    	github.repos.getCommits({
-    		user: "2016rshah", 
-    		repo: data[i].name
-    	}, function(err, result){
-    		if(err)
-    			console.log(err)
-    		else
-    		{
-    			var commits = result
-    			for(var i = 0; i<commits.length; i++)
-    			{
-    				//console.log(commits[i].commit)
-    				console.log(isodate(commits[i].commit.committer.date))
-    				datesContributed.push(isodate(commits[i].commit.commiter.date))
-    			}
-    		}
-    	})
-    }
-});
